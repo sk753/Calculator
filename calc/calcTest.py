f5cb40767babb564c839eea899b4d4def1300cd3
sk753 <78709790+sk753@users.noreply.github.com>
2021-03-30 18:56:48 -0400

    Task: Create calcTest.py

---
+++ calc/calcTest.py
@@ -0,0 +1,26 @@
+import math
+
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    result = x / y
+    return round(result, 9)
+
+
+def square(x):
+    return x ** 2
+
+
+def squareRoot(x):
+    return round(math.sqrt(x), 8)
